90e08937890d1b8f5687d57b9a8a76e381788748
Duy Le <asianinvasion.le@gmail.com>
2022-08-01 16:27:04 -0400

    getting started with Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Container, Box, Heading } from '@chakra-ui/react'
 
 const Page = () => {
-  return <div>Helllo world</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello World! I am a full stack developer
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={4}>
+          <Heading as="h2" variant="page-title">
+            Duy Le
+          </Heading>
+          <p>Digitial Designer and Developer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
