51c4af2f4d664f32315eb8458750decd90d4d4a6
Duy Le <asianinvasion.le@gmail.com>
2022-09-28 11:17:29 -0400

    added pokemon-reviews project

--- projects.js
+++ projects.js
@@ -12,7 +12,7 @@ export const projects = {
       '/projects/budget-tracker/8.png'
     ],
     description:
-      'A web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more.',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     stack: ['MongoDB', 'Express', 'React', 'NodeJS', 'Material UI', 'Vercel'],
     host: 'https://expense-app-client-seven.vercel.app/',
     git: 'https://github.com/iamdooboy/expense-app-client',
@@ -33,12 +33,29 @@ export const projects = {
     description:
       'An experimental layout transition where a stack of images animates to a gallery view, showing some more content. Recreated using Framer Motion instead of GSAP',
     stack: ['NextJS', 'CSS', 'Framer Motion', 'Vercel'],
-    host: 'https://japan-gallery.vercel.app/',
+    host: 'https://motion-gallery.vercel.app/',
     git: 'https://github.com/iamdooboy/motion-gallery',
     shortDescription:
       "A small frontend project of various photos of Japan's top 10 most popular cities. This project was created to help me understand Framer motion and React Context",
     thumbnail: '/projects/motion-gallery/thumbnail.jpg',
     imageAlt: 'Motion Gallery',
     id: 'motionGallery'
+  },
+  pokemonReview: {
+    title: 'Pokemon Reviews',
+    images: [
+      '/projects/pokemon-reviews/thumbnail.jpg',
+      '/projects/pokemon-reviews/1.jpg'
+    ],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+    stack: ['NextJS', 'Prism', 'Chakra UI', 'Vercel', 'Supabase'],
+    host: 'https://pokemon-reviews.vercel.app/',
+    git: 'https://github.com/iamdooboy/pokemon-reviews',
+    shortDescription:
+      'A place to leave reviews on your favorite/least favorite pokemon and read other peoples review [In Progress]',
+    thumbnail: '/projects/pokemon-reviews/thumbnail.jpg',
+    imageAlt: 'Pokemon-Reviews',
+    id: 'pokemonreviews'
   }
 }
