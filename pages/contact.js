24ff69e92944e5bdab5ab23a6e08e8dd37204204
Duy Le <asianinvasion.le@gmail.com>
2023-01-07 23:02:41 -0500

    update contact

--- pages/contact.js
+++ pages/contact.js
@@ -20,9 +20,9 @@ const Contact = () => {
         Get In Touch
       </Heading>
       <Text mt={3} mb={6}>
-        Although I’m not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I’ll
-        try my best to get back to you!
+        I’m currently looking for any new opportunities, my inbox is always
+        open. Whether you have a question or just want to say hi, I’ll try my
+        best to get back to you!
       </Text>
       <HookForm />
     </Container>
